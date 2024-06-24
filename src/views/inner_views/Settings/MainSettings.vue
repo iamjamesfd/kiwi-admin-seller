6c61b6adecc68345ec078066cdbd32ca5fc819a2
James Byron <jamesbyron006@gmail.com>
2024-06-24 13:19:35 +0500

    settings almost completed

--- src/views/inner_views/Settings/MainSettings.vue
+++ src/views/inner_views/Settings/MainSettings.vue
@@ -1,53 +1,75 @@
-<!-- <template>
-  <h1>Asosiy sozlamalar</h1>
-</template> -->
-
 <script setup>
-import { ref } from 'vue'
-
-// const msg = ref('Hello World!')
-
-const defaultOption = { name: '', values: [] }
-
-const options = ref([
-
-])
-
-const currentOption = ref(defaultOption)
-
-const createOptionValue = (e) => {
-  currentOption.value.values.push(e.target.value)
-  e.target.value = '';
-}
-
-const createOption = () => {
-  options.value.push(currentOption.value)
-  currentOption.value = defaultOption
-}
+import { Icon } from '@iconify/vue';
 </script>
 
 <template>
-  <div v-for="option in options" :key="option">
-    <p>u can display your options in any format you want</p>
-    <p><b>Option name:</b>{{ option.name }}</p>
-    <p><span v-for="optionValue in option.values" :key="optionValue">{{ optionValue + ', ' }}</span></p>
-    
-  </div>
-  <div>
-
+  <div class="grid grid-cols-2 gap-6 mb-8">
     <div>
-    <h3>option name</h3>
-    <input placeholder="Color, Size" v-model="currentOption.name"/>
+      <h1 class="text-sm mb-2">Do'kon nomi</h1>
+      <input
+        type="text"
+        placeholder="Dokon nomini kiriting"
+        value="UzShop qurilish materiallari do'koni"
+        class="w-full p-3 rounded-[10px] flex-1 border outline-none"
+      />
+    </div>
+    <div>
+      <h1 class="text-sm mb-2">Telefon raqam</h1>
+      <input
+        type="text"
+        placeholder="Telefon raqamni kiriting"
+        value="+998 90 838 38838"
+        class="w-full p-3 rounded-[10px] flex-1 border outline-none"
+      />
+    </div>
+    <div>
+      <h1 class="text-sm mb-2">Do'kon manzili</h1>
+      <input
+      type="text"
+      placeholder="Manzilni kiriting"
+      value="Andijon shahar 12-a uy"
+      class="w-full p-3 rounded-[10px] flex-1 border outline-none"
+      />
+    </div>
+    <div class="text-[#FF6200] flex flex-col">
+      <h1 class="invisible">clear space</h1>
+      <div class="flex items-center flex-1 gap-2 cursor-pointer">
+        <Icon icon="solar:map-linear" class="w-[18px] h-[18px]"></Icon>
+        <p>Kartadan korsatish</p>
+      </div>
+    </div>
+    <div>
+      <h1 class="text-sm mb-2">Do'kon faoliyati</h1>
+      <input
+        disabled
+        type="text"
+        placeholder="Maishiy texnika"
+        class="w-full p-3 rounded-[10px] flex-1 border outline-none"
+      />
+    </div>
+    <div>
+      <h1 class="text-sm mb-2">Qo'shilgan sanasi</h1>
+      <input
+      disabled
+      type="text"
+      placeholder="24 Noyabr, 2022"
+      class="w-full p-3 rounded-[10px] flex-1 border outline-none"
+      />
+    </div>
+    <div>
+      <h1 class="text-sm mb-2">Ish vaqti</h1>
+      <input
+      type="text"
+      placeholder="09:00 - 18:00"
+      class="w-full p-3 rounded-[10px] flex-1 border outline-none"
+      />
+    </div>
   </div>
-
   <div>
-    <h3>option values</h3>
-    <p v-for="optionValue in currentOption.values" :key="optionValue">
-      <input :value="optionValue"  />
-    </p>
-    <input @keydown.enter="createOptionValue" />
+    <h1 class="text-sm mb-2">Do'kon haqida qisqacha</h1>
+    <textarea class="resize-none border w-full h-[200px] outline-none rounded-lg p-3"></textarea>
   </div>
-  <br>
-  <button @click="createOption">submit</button>
+  <div class="flex justify-end mt-8">
+    <button class="bg-[#FF6200] text-white py-[10px] px-[32px] rounded-lg">Saqlash</button>
   </div>
 </template>
--- src/views/inner_views/Settings/ProfileSettings.vue
+++ src/views/inner_views/Settings/ProfileSettings.vue
@@ -1,3 +1,56 @@
 <template>
-  <h1>Profil sozlamalari</h1>
+  <h1 class="text-lg font-semibold mb-5">Sizning ma'lumotlaringiz</h1>
+  <div class="grid grid-cols-1 lg:grid-cols-2">
+    <div class="mb-7">
+      <h1 class="text-sm mb-2">To'liq ismingiz</h1>
+      <input
+        type="text"
+        placeholder="Ism familiya"
+        value="Rahmatullo Madrahimov"
+        class="w-full p-3 rounded-[10px] border outline-none"
+      />
+    </div>
+    <div class="hidden lg:block lg:invisible">clear space</div>
+    <div  class="mb-7">
+      <h1 class="text-sm mb-2">Telefon raqam</h1>
+      <input
+        type="text"
+        placeholder="Telefon raqam"
+        value="+998 90 000 0000"
+        class="w-full p-3 rounded-[10px] border outline-none"
+      />
+    </div>
+  </div>
+  <div>
+    <h1 class="text-lg font-semibold mb-5">Bildirishnomalar</h1>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div class="flex items-center justify-between">
+        <p>Push-bildirishnomalar</p>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider"></span>
+        </label>
+      </div>
+      <div class="hidden lg:block lg:invisible ">clear space</div>
+      <div class="flex items-center justify-between">
+        <p>Yangiliklar</p>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider"></span>
+        </label>
+      </div>
+      <div class="hidden lg:block lg:invisible ">clear space</div>
+      <div class="flex items-center justify-between">
+        <p>Chatlar</p>
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider"></span>
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-end gap-6 mt-8">
+    <button class="bg-slate-200 py-[10px] px-[32px] rounded-lg">Bekor qilish</button>
+    <button class="bg-[#FF6200] text-white py-[10px] px-[32px] rounded-lg">Saqlash</button>
+  </div>
 </template>
