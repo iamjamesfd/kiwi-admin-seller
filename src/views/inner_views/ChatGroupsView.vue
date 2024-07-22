071b4951338f0716c173619f1065ff6767a21423
James Byron <jamesbyron006@gmail.com>
2024-07-22 18:01:58 +0500

    something wrong with chat

--- src/views/inner_views/ChatGroupsView.vue
+++ src/views/inner_views/ChatGroupsView.vue
@@ -1,26 +1,21 @@
 <template>
   <DashboardHeader title="Chat" />
- <div class="gap-x-5 py-5 md:grid md:grid-cols-9">
-    <div class="bg-white px-4 p-4 border-b">
-      <label for="search-input" class="bg-[#F5F5F5] flex gap-3 items-center p-2 rounded-lg">
-        <Icon icon="iconamoon:search" class="w-6 h-6"></Icon>
-        <input type="text" placeholder="Qidiruv..." id="search-input" class="flex-1 outline-none bg-transparent">
-      </label>
+  <div class="gap-x-5 md:grid md:grid-cols-9">
+    <div class="col-span-9">
+      <div class="bg-white px-4 p-4 border-b">
+        <label for="search-input" class="bg-[#F5F5F5] flex gap-3 items-center p-2 rounded-lg">
+          <Icon icon="iconamoon:search" class="w-6 h-6"></Icon>
+          <input type="text" placeholder="Qidiruv..." id="search-input" class="flex-1 outline-none bg-transparent">
+        </label>
+      </div>
+      <ChatList :class="{ hidden: chatOpen }"
+        class="border-stroke_primary md:col-span-12 md:overflow-hidden md:rounded-lg md:border lg:col-span-4 lg:block" />
     </div>
-    <ChatList
-      :class="{ hidden: chatOpen }"
-      class="border-stroke_primary md:col-span-12 md:overflow-hidden md:rounded-lg md:border lg:col-span-4 lg:block"
-    />
     <router-view v-slot="{ Component }">
-      <div
-        :class="{ 'hidden lg:border': !chatOpen }"
-        class="overflow-hidden border-stroke_primary md:col-span-12 md:rounded-lg md:border lg:col-span-5 lg:block"
-      >
+      <div :class="{ 'hidden lg:border': !chatOpen }"
+        class="overflow-hidden border-stroke_primary md:col-span-12 md:rounded-lg md:border lg:col-span-5 lg:block">
         <component :is="Component" />
-        <div
-          v-if="!Component"
-          class="flex h-[588px] items-center justify-center"
-        >
+        <div v-if="!Component" class="flex h-[588px] items-center justify-center">
           <div class="flex flex-col items-center space-y-4 *:text-[#8CAAC9]">
             <Icon icon="ant-design:message-outlined" height="64" />
             <p class="text-xl">Chatni tanlang</p>
@@ -52,4 +47,3 @@ watch(
   }
 );
 </script>
-
